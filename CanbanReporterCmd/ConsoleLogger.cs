44a65fa906f40e059344d6320be01e1eeb82a5b5
pedias <pedias@microsoft.com>
2019-05-08 07:47:25 +0200

    Implement  ILogger and ISettings (Work in Progress)

---
+++ CanbanReporterCmd/ConsoleLogger.cs
@@ -0,0 +1,33 @@
+﻿using KanbanReporter.Business.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace CanbanReporterCmd
+{
+    public class ConsoleLogger : ILogger
+    {
+        public void Enter(object sender, [CallerMemberName] string methodName = "", params object[] args)
+        {
+            var arguments = string.Join(',', args.Select(a => $"'{a}'"));
+            Console.WriteLine(sender.ToString() + $"{methodName}({arguments})");
+        }
+
+        public void LogError(string errorMessage, Exception exception = null, [CallerMemberName] string source = null)
+        {
+            Console.WriteLine(">>> ERROR >>> " + $"{source}: {errorMessage}");
+        }
+
+        public void LogInfo(string message)
+        {
+            Console.WriteLine($"[Info]{message}");
+        }
+
+        public void LogWarning(string message)
+        {
+            Console.WriteLine($"[Warning]{message}");
+        }
+    }
+}
