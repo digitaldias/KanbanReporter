a09a5686caaf74a4b9c2d270a2a29e9a283c7410
pedias <pedias@microsoft.com>
2019-05-08 07:09:19 +0200

    Introduce KanbanReporterCmd, a command line alternative to the Azure Function

---
+++ CanbanReporterCmd/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace CanbanReporterCmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("KanbanReporter Tool " + GetAssemblyVersionString());
+        }
+
+        private static string GetAssemblyVersionString()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+            return string.Format("{0}.{1}", fileVersionInfo.ProductMajorPart, fileVersionInfo.FileMinorPart);
+        }
+    }
+}
